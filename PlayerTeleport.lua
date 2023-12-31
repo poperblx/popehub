a692c4d8dbaa882613b6996213ab77b296339244
poperblx <pastorhokage069@gmail.com>
2023-12-31 15:42:27 +0800

    fix typo error in PlayerTeleport module

--- PlayerTeleport.lua
+++ PlayerTeleport.lua
@@ -2,14 +2,14 @@ local remote = game:GetService("ReplicatedStorage").Remote;
 local player = game.Players.LocalPlayer;
 local PlayerTeleport = {};
 
-function playerTeleport.getCurrentPlayerPos()
+function PlayerTeleport.getCurrentPlayerPos()
     if player.Character then
         return player.Character.HumanoidRootPart.Position;
     end
     return false;
 end
 
-function playerTeleport.teleportTo(placeCFrame)
+function PlayerTeleport.teleportTo(placeCFrame)
     if player.Character then
         player.Character:WaitForChild("HumanoidRootPart");
         player.Character.HumanoidRootPart.CFrame = placeCFrame;
